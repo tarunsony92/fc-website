47fdd40a156f2d0d5d584efbff8331fffb70d6be
tarunsony92 <tarunsoni210426@gmail.com>
2025-06-17 15:18:08 +0530

    Fix: updated ALLOWED_HOSTS for Render

--- fc/settings.py
+++ fc/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 
 # Allowed Hosts
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
+ALLOWED_HOSTS = ['fc-website-ni2h.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
